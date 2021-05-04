39049f31a9e531078008e93ce29e01958756c6d7
Malachi <malachi.mackie@hill-labs.co.nz>
2021-05-04 13:28:59 +1200

    Reproducible example

---
+++ CustomControlWithContentProperty.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace XamlPreviewBugContentProperty
+{
+    [ContentProperty(nameof(MyCustomContent))]
+    public partial class CustomControlWithContentProperty : UserControl
+    {
+        public CustomControlWithContentProperty()
+        {
+            InitializeComponent();
+        }
+
+        public static DependencyProperty CustomContentProperty = DependencyProperty.Register("MyCustomContent",
+            typeof(FrameworkElement), typeof(CustomControlWithContentProperty));
+
+        public FrameworkElement MyCustomContent
+        {
+            get => (FrameworkElement)GetValue(CustomContentProperty);
+            set => SetValue(CustomContentProperty, value);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace XamlPreviewBugContentProperty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
